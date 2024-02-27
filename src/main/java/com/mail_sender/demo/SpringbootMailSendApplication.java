25d41bdbfdff1b8d4508bb2a4518560db6675a97
pasindu <www.pasindudenuka5770@gmail.com>
2024-02-27 20:01:29 +0530

    Spring Boot project for mail sending

---
+++ src/main/java/com/mail_sender/demo/SpringbootMailSendApplication.java
@@ -0,0 +1,13 @@
+package com.mail_sender.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMailSendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMailSendApplication.class, args);
+	}
+
+}
